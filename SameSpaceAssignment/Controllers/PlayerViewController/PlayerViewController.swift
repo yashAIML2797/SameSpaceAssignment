9c2b4da9c48097e3bed8da42b9caafe0e2039d96
Yash Uttekar <yash.uttekar2797@icloud.com>
2023-10-02 09:28:17 +0530

    AVPlayerItemDidPlayToEndTime play automatic

--- SameSpaceAssignment/Controllers/PlayerViewController/PlayerViewController.swift
+++ SameSpaceAssignment/Controllers/PlayerViewController/PlayerViewController.swift
@@ -178,6 +178,8 @@ class PlayerViewController: UIViewController {
         previousButton.addTarget(self, action: #selector(handlePreviousButtonAction), for: .touchUpInside)
         
         configurePlayer()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didFinishCurrentTrack), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
     override func viewDidLayoutSubviews() {
@@ -203,6 +205,7 @@ class PlayerViewController: UIViewController {
     
     deinit {
         print("deallocated")
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
     func configure(with song: Song) {
@@ -273,5 +276,24 @@ class PlayerViewController: UIViewController {
         } else {
             start()
         }
+    }
+    
+    @objc func didFinishCurrentTrack(notification: Notification) {
+        
+        if let layout = coverFlowView.collectionViewLayout as? CoverFlowViewLayout {
+            if layout.currentItemIdex < (songs.count - 1) {
+                layout.currentItemIdex += 1
+            } else {
+                layout.currentItemIdex = songs.count - 1
+            }
+            let offsetX = layout.getTargetOffsetX(for: layout.currentItemIdex)
+            self.coverFlowView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: true)
+
+            let song = songs[layout.currentItemIdex]
+            self.currentPlayingSong = song
+            self.configure(with: song)
+        }
+        
+        start()
     }
 }
--- SameSpaceAssignment/Views/MinimizedPlayerView.swift
+++ SameSpaceAssignment/Views/MinimizedPlayerView.swift
@@ -51,10 +51,15 @@ class MinimizedPlayerView: UIView {
         super.init(frame: frame)
         setupViews()
         addGesture()
+        NotificationCenter.default.addObserver(self, selector: #selector(didFinishCurrentTrack), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
     private func setupViews() {
@@ -129,6 +134,28 @@ class MinimizedPlayerView: UIView {
     @objc func handleTap(gesture: UITapGestureRecognizer) {
         if let song = currentPlayingSong {
             launchPlayerDelegate?.launchPlayer(with: songs, startingSong: song)
+        }
+    }
+    
+    @objc func didFinishCurrentTrack(notification: Notification) {
+        
+        if let currentPlayingSong = currentPlayingSong,
+           let currentIndex = songs.firstIndex(where: {$0.id == currentPlayingSong.id})
+        {
+            var nextSongIndex = currentIndex
+            
+            if currentIndex < (songs.count - 1) {
+                nextSongIndex += 1
+            } else {
+                nextSongIndex = songs.count - 1
+            }
+            
+            let nextSong = songs[nextSongIndex]
+            self.currentPlayingSong = nextSong
+            self.configure(with: nextSong)
+            if let url = URL(string: nextSong.url) {
+                AudioManager.shared.start(itemURL: url)
+            }
         }
     }
 }
